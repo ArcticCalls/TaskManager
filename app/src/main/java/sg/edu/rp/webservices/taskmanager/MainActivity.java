159996b65225f28843e8cc00166e885c061bbeb4
19045104 <19045104@myrp.edu.sg>
2021-07-29 13:59:13 +0800

    5 seconds later

--- app/src/main/java/sg/edu/rp/webservices/taskmanager/MainActivity.java
+++ app/src/main/java/sg/edu/rp/webservices/taskmanager/MainActivity.java
@@ -2,14 +2,19 @@ package sg.edu.rp.webservices.taskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
     Button btnAddTask;
-
+    int reqCode = 12345;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
                 Intent i = null;
                 i = new Intent(MainActivity.this, AddActivity.class);
                 startActivity(i);
+
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.SECOND, 5);
+
+                Intent intent = new Intent(MainActivity.this, fivesecondlater.class);
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, reqCode, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+                AlarmManager am = (AlarmManager)getSystemService(Activity.ALARM_SERVICE);
+                am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pendingIntent);
 
             }
         });
