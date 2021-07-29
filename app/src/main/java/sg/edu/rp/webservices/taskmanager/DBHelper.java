ff471bb06f4da2e97d342969237263024deb1c07
19041582-Qayyum-Danish-Tan <19041582@myrp.edu.sg>
2021-07-29 14:35:24 +0800

    update

--- app/src/main/java/sg/edu/rp/webservices/taskmanager/DBHelper.java
+++ app/src/main/java/sg/edu/rp/webservices/taskmanager/DBHelper.java
@@ -26,7 +26,7 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String createNoteTableSql = "CREATE TABLE " + TABLE_TASK + "("
-                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_TASK_NAME + "TEXT"
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_TASK_NAME + " TEXT,"
                 + COLUMN_DESCRIPTION + " TEXT ) ";
         db.execSQL(createNoteTableSql);
 
