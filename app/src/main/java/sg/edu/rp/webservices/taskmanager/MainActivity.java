c3698f542298099ac706af12bd809c2ec71fc12f
19016011 <19016011@rp.edu.sg>
2021-07-29 13:38:33 +0800

    commit

--- app/src/main/java/sg/edu/rp/webservices/taskmanager/MainActivity.java
+++ app/src/main/java/sg/edu/rp/webservices/taskmanager/MainActivity.java
@@ -2,13 +2,44 @@ package sg.edu.rp.webservices.taskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lvTask;
+    Button btnAddTask;
+    ArrayAdapter aa;
+    ArrayList<Tasks> tasksList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvTask = findViewById(R.id.lvTask);
+        btnAddTask = findViewById(R.id.btnAddTask);
+
+        tasksList = new ArrayList<Tasks>();
+        DBHelper dbh = new DBHelper(MainActivity.this);
+        tasksList.addAll(dbh.getAllNotes());
+        dbh.close();
+        aa = new TaskAdapter(MainActivity.this, R.layout.row, tasksList);
+        lvTask.setAdapter(aa);
+
+
+        btnAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/sg/edu/rp/webservices/taskmanager/Tasks.java
+++ app/src/main/java/sg/edu/rp/webservices/taskmanager/Tasks.java
@@ -7,11 +7,23 @@ import java.io.Serializable;
 public class Tasks implements Serializable {
 
     private int id;
+    private String name;
     private String desc;
+    private int seconds;
 
-    public Tasks(int id, String desc){
+    public Tasks(int id, String desc, String name, int seconds){
         this.id = id;
         this.desc = desc;
+        this.name = name;
+        this.seconds = seconds;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getId(){
@@ -30,9 +42,15 @@ public class Tasks implements Serializable {
         this.desc = desc;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
 
     @Override
     public String toString() {
-        return id + "\n" + desc;
+        return id + "\n" + desc + "\n" + name + "\n" + seconds;
     }
 }
