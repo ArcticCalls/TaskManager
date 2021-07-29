ac06c5adf0e0bb4a903d8d2f14ed0195a52af4eb
19041582-Qayyum-Danish-Tan <19041582@myrp.edu.sg>
2021-07-29 13:40:46 +0800

    update

--- app/src/main/java/sg/edu/rp/webservices/taskmanager/MainActivity.java
+++ app/src/main/java/sg/edu/rp/webservices/taskmanager/MainActivity.java
@@ -2,13 +2,29 @@ package sg.edu.rp.webservices.taskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnAddTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnAddTask = findViewById(R.id.btnAddTask);
+        btnAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = null;
+                i = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
